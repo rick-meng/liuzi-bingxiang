c0b48f6325958475eca7bd22987811d931ee221c
rick-meng <wanglimengfm@163.com>
2026-02-15 23:35:23 +1300

    chore: stop tracking dist build artifacts

--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "./app.js";
-const port = Number(process.env.PORT ?? 3000);
-const app = createApp();
-app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server running on http://localhost:${port}`);
-});
